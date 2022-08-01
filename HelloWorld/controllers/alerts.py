ac49b03e80703f297a518f0dea9de2d773e24cab
Rohit Kolapkar <rohit.j.kolapkar@seagate.com>
2022-08-01 07:54:28 -0600

    alerts payload added
    Signed-off-by: Rohit Kolapkar <rohit.j.kolapkar@seagate.com>

--- HelloWorld/controllers/alerts.py
+++ HelloWorld/controllers/alerts.py
@@ -8,4 +8,16 @@ class AlertsView(HelloWorldView):
         pass
 
     async def get(self):
-        return {}
+        list_response = list()
+        response = {
+            "time": "1589956569",
+            "alert_uuid": "15899565680a19bcfadbff47a483bc653fa5cca1d2",
+            "description": "Seagate Storage Platform Library - Sensor Response",
+            "severity": "critical",
+            "state": "fault",
+            "acknowledged": False,
+            "resolved": False,
+            "hostname": "ssc-vm-g4-rhev4-0392.colo.seagate.com"
+        }
+        list_response.append(response)
+        return {"alerts":list_response}
